9546838ac0aa8a368c2bfeec585829d4308a698a
fred-gutierrez <fredsg22@hotmail.com>
2024-02-09 15:36:44 -0600

    Added dap-ui reset, and fixed the missing opts in the overrides.lua file

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -3,7 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "bash",
         "html",
         "javascript",
         "json",
@@ -16,32 +15,33 @@ return {
         "tsx",
         "typescript",
         "vim",
-        "yaml",
       },
     },
   },
   {
     "williamboman/mason.nvim",
-    ensure_installed = {
-      -- LSP's
-      "eslint-lsp",
-      "svelte-language-server",
-      "json-lsp",
-      "css-lsp",
-      "cssmodules-language-server",
-      "html-lsp",
-      "lua-language-server",
-      "tailwindcss-language-server",
-      "typescript-language-server",
-      "vue-language-server",
-      "clangd",
-      -- Linters
-      "eslint_d",
-      -- Formatters
-      "sql-formatter",
-      "clang-format",
-      "stylua",
-      "prettier",
+    opts = {
+      ensure_installed = {
+        -- LSP's
+        "eslint-lsp",
+        "svelte-language-server",
+        "json-lsp",
+        "css-lsp",
+        "cssmodules-language-server",
+        "html-lsp",
+        "lua-language-server",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "vue-language-server",
+        "clangd",
+        -- Linters
+        "eslint_d",
+        -- Formatters
+        "sql-formatter",
+        "clang-format",
+        "stylua",
+        "prettier",
+      },
     },
   },
 }
